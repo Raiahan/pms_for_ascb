346b65fd08e8341309ef940b30a5f6e64be793f3
Mario Villani <villani.mario90@gmail.com>
2014-06-04 23:47:58 +0200

    Sistemato layout installazione
    Aggiornato markup di installazione.

--- installazione/cambia_nome.php
+++ installazione/cambia_nome.php
@@ -57,61 +57,56 @@ function installa_pms() {
     return "Installazione completata con successo!Ora puoi cancellare la cartella installazione e utilizzare PMS con i dati forniti.";
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="it">
-    <head>
+<!doctype html>
+<html lang="it"><head>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8">
         <meta charset="utf-8">
         <title>Installazione PMS</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Pagina installazione">
         <meta name="author" content="Mario Villani">
-        <link rel="stylesheet" href="../bootstrap/css/bootstrap-responsive.min.css"/>
-        <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css"/>
-        <link rel="stylesheet" href="../css/index.css"/>
-        <!-- Font awesome -->
-        <link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/index.css">
+        <link rel="stylesheet" href="../css/comune.css">
         <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
         <!--[if lt IE 9]>
         <script src="../assets/js/html5shiv.js"></script>
         <![endif]-->
     </head>
-
     <body>
         <div id="wrap">
             <div class="container-fluid text-center">
-                <div class="row-fluid">
-                    <div class="span12">
-                        <img id="logo" src="../logo.jpg" />
+                <div class="row">
+                    <div class="col-sm-12 col-md-12">
+                        <img class="img-responsive" id="logo" src="../logo.png">
                     </div>
                 </div>
                 <div class="row">
                     <form id="installform" class="form-signin" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
                         <h2 class="form-signin-heading">Installazione</h2>
-                        <input class="input-block-level" name="email" placeholder="Email del master" type="text" />
-                        <input class="input-block-level" name="username" placeholder="Username del master" type="text" />
-                        <input class="input-block-level" name="password" placeholder="Password master" type="text" />
-                        <input type="hidden" name="install" />
+                        <input class="form-control" name="email" placeholder="Email del master" type="text">
+                        <input class="form-control" name="username" placeholder="Username del master" type="text">
+                        <input class="form-control" name="password" placeholder="Password master" type="text">
+                        <input class="form-control" name="install" type="hidden">
                         <?php if (isset($messaggio)) {
                             ?>
-                            <div class="alert alert-info">
+                            <div class="alert alert-info alert-dismissable">
                                 <button type="button" class="close" data-dismiss="alert">
-                                    &times;
+                                    ×
                                 </button>
                                 <?php echo $messaggio ?>
                             </div>
                         <?php } ?>
-                        <button class="btn btn-large btn-primary" type="submit" id="installa">
+                        <button class="btn btn-primary btn-lg" type="submit" id="installa">
                             Installa
-                        </button><br /><br />
+                        </button><br><br>
                     </form>  
                 </div>
             </div><div id="push"></div></div>
             <?php
             require "../componenti/footer.php";
             ?>
-        <script type="text/javascript" src="../librerie/jquery-1.10.2.min.js"></script>
+        <script type="text/javascript" src="../librerie/jquery.js"></script>
         <script src="../bootstrap/js/bootstrap.min.js"></script>
         <script>
             $(document).ready(function() {
