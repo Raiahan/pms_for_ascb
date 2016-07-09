600f65c32d9b5a4e13f309c4b52d9ef600981a38
Mario Villani <villani.mario90@gmail.com>
2016-07-09 21:36:54 +0200

    css/ and js/ prettify
    Prettified sources.

--- js/contatta.js
+++ js/contatta.js
@@ -3,34 +3,34 @@
  */
 
 $(document).ready(function() {
-	//Assegno le azioni ai pulsanti
-	$('#pulsante_invia').bind('click', function() {
-		inviaMail();
-	});
+    //Assegno le azioni ai pulsanti
+    $('#pulsante_invia').bind('click', function() {
+        inviaMail();
+    });
 });
 
 function inviaMail() {
-	var oggetto = $('#oggetto').val();
-	var corpo = $('#corpo').val();
-	var notifica = lanciaNotifica("Invio della mail in corso", "info");
-	var opzioni = {
-		type : "POST",
-		url : "scripts/utente/contatta_master.php",
-		beforeSend : function(xhr) {
-			$('#pulsante_invia').attr("disabled", "disabled");
-		},
-		data : {
-			oggetto : oggetto,
-			corpo : corpo
-		},
-		success : function(messaggio) {
-			lanciaNotifica(messaggio, "success");
-			$("#pulsante_invia").removeAttr("disabled");
-		},
-		error : function(XMLHTTP) {
-			lanciaNotifica(XMLHTTP.responseText, "error");
-			$("#pulsante_invia").removeAttr("disabled");
-		}
-	};
-	$.ajax(opzioni);
+    var oggetto = $('#oggetto').val();
+    var corpo = $('#corpo').val();
+    var notifica = lanciaNotifica("Invio della mail in corso", "info");
+    var opzioni = {
+        type: "POST",
+        url: "scripts/utente/contatta_master.php",
+        beforeSend: function(xhr) {
+            $('#pulsante_invia').attr("disabled", "disabled");
+        },
+        data: {
+            oggetto: oggetto,
+            corpo: corpo
+        },
+        success: function(messaggio) {
+            lanciaNotifica(messaggio, "success");
+            $("#pulsante_invia").removeAttr("disabled");
+        },
+        error: function(XMLHTTP) {
+            lanciaNotifica(XMLHTTP.responseText, "error");
+            $("#pulsante_invia").removeAttr("disabled");
+        }
+    };
+    $.ajax(opzioni);
 }
--- js/messaggio.js
+++ js/messaggio.js
@@ -3,37 +3,37 @@
  */
 
 $(document).ready(function() {
-	//Assegno le azioni ai pulsanti
-	$('#pulsante_messaggio').bind('click', function() {
-		inviaMessaggio();
-	});
+    //Assegno le azioni ai pulsanti
+    $('#pulsante_messaggio').bind('click', function() {
+        inviaMessaggio();
+    });
 });
 
 function inviaMessaggio() {
-	var corpo = $('#corpo').val();
-	var invia = 0;
-	if ($('#invia').is(':checked')) {
-		invia = 1;
-	}
-	var notifica = lanciaNotifica("Invio messaggio in corso", "info");
-	var opzioni = {
-		type : "POST",
-		url : "../scripts/messaggio/invia_messaggio.php",
-		beforeSend : function(xhr) {
-			$('#pulsante_messaggio').attr("disabled", "disabled");
-		},
-		data : {
-			corpo : corpo,
-			invia : invia
-		},
-		success : function(messaggio) {
-			lanciaNotifica(messaggio, "success");
-			$("#pulsante_messaggio").removeAttr("disabled");
-		},
-		error : function(XMLHTTP) {
-			lanciaNotifica(XMLHTTP.responseText, "error");
-			$("#pulsante_messaggio").removeAttr("disabled");
-		}
-	};
-	$.ajax(opzioni);
+    var corpo = $('#corpo').val();
+    var invia = 0;
+    if ($('#invia').is(':checked')) {
+        invia = 1;
+    }
+    var notifica = lanciaNotifica("Invio messaggio in corso", "info");
+    var opzioni = {
+        type: "POST",
+        url: "../scripts/messaggio/invia_messaggio.php",
+        beforeSend: function(xhr) {
+            $('#pulsante_messaggio').attr("disabled", "disabled");
+        },
+        data: {
+            corpo: corpo,
+            invia: invia
+        },
+        success: function(messaggio) {
+            lanciaNotifica(messaggio, "success");
+            $("#pulsante_messaggio").removeAttr("disabled");
+        },
+        error: function(XMLHTTP) {
+            lanciaNotifica(XMLHTTP.responseText, "error");
+            $("#pulsante_messaggio").removeAttr("disabled");
+        }
+    };
+    $.ajax(opzioni);
 }
